8dec8d335cd0f1d00e6d9169c91a3a6fa33af599
BadOfficer <tarasbondarenko2005@gmail.com>
2026-04-16 23:19:14 +0300

    Solution

--- src/validateEmail.test.js
+++ src/validateEmail.test.js
@@ -7,11 +7,43 @@ describe(`Function 'validateEmail':`, () => {
     expect(validateEmail).toBeInstanceOf(Function);
   });
 
-  it(`should return boolean`, () => {});
+  it(`should return boolean`, () => {
+    expect(typeof validateEmail('test@example.com')).toBe('boolean');
+  });
 
   it(`should return 'true' for the valid email`, () => {
     expect(validateEmail('test838@gmail.com.')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'true' for the 'test@mail.com'`, () => {
+    expect(validateEmail('test@mail.com')).toBeTruthy();
+  });
+
+  it(`should return 'false' for the 'false@email`, () => {
+    expect(validateEmail('false@email')).toBeFalsy();
+  });
+
+  it(`should return 'true' for 't@q.c'`, () => {
+    expect(validateEmail('t@q.c')).toBeTruthy();
+  });
+
+  it(`should return false for not english letters`, () => {
+    expect(validateEmail('тест@приклад.ком')).toBeFalsy();
+  });
+
+  it(`should return 'false' for dot at the start`, () => {
+    expect(validateEmail('.test@example.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for dot at the end`, () => {
+    expect(validateEmail('test.@example.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for double dots`, () => {
+    expect(validateEmail('tes..t@example.com')).toBeFalsy();
+  });
+
+  it(`should return 'false' for special symbol`, () => {
+    expect(validateEmail('t!est@example.com')).toBeFalsy();
+  });
 });
